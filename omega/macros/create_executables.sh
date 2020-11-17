def00947b573fc303221548e14e1e94f3a480e11
beyerja <beyerja@gmx.de>
2020-11-17 10:26:37 +0100

    Separate helper functions from O'Mega core
    - Separate fortran helper functions from O'Mega core.

--- omega/macros/create_executables.sh
+++ omega/macros/create_executables.sh
@@ -12,6 +12,7 @@ dir="$( cd "$( dirname "${BASH_SOURCE[0]}"  )" && pwd  )"
 
 src_dir="${dir}/../src"
 omega_dir="${src_dir}/OMega"
+helper_dir="${src_dir}/Helpers"
 processes_dir="${src_dir}/Processes"
 
 # Output directories
@@ -29,7 +30,7 @@ fi
 process_files=$(find ${processes_dir} -iname "*.f90" -print)
 
 # ---------------------------------------------------------------------------- #
-# O'Mega files need to be listed in correct order to compile
+# Fortran base files need to be listed in correct order to compile
 omega_files=(
 omega_kinds.f90
 omega_constants.f90
@@ -43,9 +44,12 @@ omega_couplings.f90
 omega_spinor_couplings.f90
 omega_utils.f90
 omega95.f90
-MathOperations.f90
 ww_sl0eq.f90
 ww_sl0muq.f90
+)
+
+helper_files=(
+MathOperations.f90
 )
 
 # ---------------------------------------------------------------------------- #
@@ -59,6 +63,15 @@ do
   gfortran -c ${omega_dir}/${omega_file}
 done
 echo "Done compiling O'Mega."
+
+echo "Start compiling Helpers."
+cd ${lib_dir}
+for helper_file in ${helper_files[@]};
+do
+  echo "Compiling into library: ${helper_file}"
+  gfortran -c ${helper_dir}/${helper_file}
+done
+echo "Done compiling Helpers."
 
 libaries=$(find ${lib_dir} -iname "*.o" -print)
 ar -qs "${lib_dir}/libomega.a" $libaries
@@ -79,6 +92,9 @@ echo "Done compiling processes."
 # ---------------------------------------------------------------------------- #
 # Clean up
 
+echo "Cleaning up intermediate libraries."
 rm ${lib_dir}/*
 
 # ---------------------------------------------------------------------------- #
+
+echo "Done!"
