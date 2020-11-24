9a33d2b961a532218e3cef0a06647ca9f0be30ee
beyerja <beyerja@gmx.de>
2020-11-24 11:18:45 +0100

    Switch fortran helpers to O'Mega precision
    - Use the O'Mega internal floatin point precision in the fortran 
    helpers.

--- src/fortran/Helpers/MathOperations.f90
+++ src/fortran/Helpers/MathOperations.f90
@@ -2,19 +2,21 @@ module MathOperations
   public :: lorentzBOOST,SphereCoordinate
 contains  
 subroutine lorentzBOOST( boost, MyVector, BoostedVector) ! Ein Unterprogramm des Typs subroutine	
+  ! Other modules
+  use omega_kinds ! Only for O'Mega precision
 	! Deklaration der formalen Parameter
-	implicit none 
-	real(kind=8), dimension(0:3), intent(in)  :: boost, MyVector
-	real(kind=8), dimension(0:3), intent(out) :: BoostedVector
-	real(kind=8), dimension(0:3,0:3) :: LorentzMatrix
-	real(kind=8), dimension(1:3) :: direction
-	real(kind=8) :: lg,gb,currentp
+  implicit none 
+	real(kind=omega_prec), dimension(0:3), intent(in)  :: boost, MyVector
+	real(kind=omega_prec), dimension(0:3), intent(out) :: BoostedVector
+	real(kind=omega_prec), dimension(0:3,0:3) :: LorentzMatrix
+	real(kind=omega_prec), dimension(1:3) :: direction
+	real(kind=omega_prec) :: lg,gb,currentp
 	integer :: i,j
 	currentp=0
 	do i=1,3
-		currentp=currentp + boost(i)**2._8
+		currentp=currentp + boost(i)**2._omega_prec
 	end do
-	lg = boost(0) / sqrt( boost(0)**2._8 - currentp )
+	lg = boost(0) / sqrt( boost(0)**2._omega_prec - currentp )
 	currentp=sqrt( currentp )
 	gb = sqrt( lg**2 - 1 )
 	! print *,'Momentum, Gamma, gammabeta ', currentp, lg, gb
@@ -25,12 +27,12 @@ subroutine lorentzBOOST( boost, MyVector, BoostedVector) ! Ein Unterprogramm des
 	LorentzMatrix=0
 	LorentzMatrix(0,0)=lg
 	do i=1,3
-		LorentzMatrix(0,i) = -1._8*gb*direction(i)
+		LorentzMatrix(0,i) = -1._omega_prec*gb*direction(i)
 		LorentzMatrix(i,0) = LorentzMatrix(0,i)
 		do j=1,3
-			LorentzMatrix(j,i) = ( lg - 1._8) * direction(i) * direction(j)
+			LorentzMatrix(j,i) = ( lg - 1._omega_prec) * direction(i) * direction(j)
 		end do
-		LorentzMatrix(i,i) = LorentzMatrix(i,i) + 1._8
+		LorentzMatrix(i,i) = LorentzMatrix(i,i) + 1._omega_prec
 	end do
 	BoostedVector=0
 	! BoostedVector = boost
@@ -46,8 +48,10 @@ subroutine lorentzBOOST( boost, MyVector, BoostedVector) ! Ein Unterprogramm des
 end subroutine lorentzBOOST
 
 pure function SphereCoordinate( magnitude, theta, phi ) result (Cartesian)
-	real(kind=8), intent(in)  :: magnitude, theta, phi
-	real(kind=8), dimension(3) :: Cartesian
+  ! Other modules
+  use omega_kinds ! Only for O'Mega precision
+	real(kind=omega_prec), intent(in)  :: magnitude, theta, phi
+	real(kind=omega_prec), dimension(3) :: Cartesian
 	Cartesian=0
 	Cartesian(1) = magnitude*sin(theta)*cos(phi)
 	Cartesian(2) = magnitude*sin(theta)*sin(phi)
--- src/fortran/Helpers/Rotation.f90
+++ src/fortran/Helpers/Rotation.f90
@@ -6,8 +6,13 @@ contains
 
 function cross(a, b)
   ! Function calculates and returns the cross product of two vectors
-  real(kind=8), dimension(3) :: cross
-  real(kind=8), dimension(3), INTENT(IN) :: a, b
+  
+  ! Other modules
+  use omega_kinds ! Only for O'Mega precision
+  
+  ! Variable definitions
+  real(kind=omega_prec), dimension(3) :: cross
+  real(kind=omega_prec), dimension(3), INTENT(IN) :: a, b
 
   cross(1) = a(2) * b(3) - a(3) * b(2)
   cross(2) = a(3) * b(1) - a(1) * b(3)
@@ -18,21 +23,26 @@ function rotate_out_of(x_in,z_in) result(rotation)
   ! Function calculates the rotation matrix.
   ! z point already in the right direction, x may need to be adjusted.
   ! Rotation matrix can be used to rotate _out_ of the given coordinate system.
-  real(kind=8), dimension (3,3) :: rotation
-  real(kind=8), dimension(9) :: rotvec
-  real(kind=8), dimension(3), INTENT(IN) :: x_in, z_in
-  real(kind=8), dimension(3) :: x, y, z
+  
+  ! Other modules
+  use omega_kinds ! Only for O'Mega precision
+  
+  ! Variable definitions
+  real(kind=omega_prec), dimension (3,3) :: rotation
+  real(kind=omega_prec), dimension(9) :: rotvec
+  real(kind=omega_prec), dimension(3), INTENT(IN) :: x_in, z_in
+  real(kind=omega_prec), dimension(3) :: x, y, z
 
   ! Normalise the z axis
-  z = 1.0/norm2(z_in) * z_in
+  z = 1._omega_prec/norm2(z_in) * z_in
   
   ! Get the y axis -> Cross product of z and x
   y = cross(z,x_in)
-  y = 1.0/norm2(y) * y
+  y = 1._omega_prec/norm2(y) * y
   
   ! Now determine correct x axis -> y cross z
   x = cross(y,z)
-  x = 1.0/norm2(x) * x
+  x = 1._omega_prec/norm2(x) * x
 
   ! Build rotation matrix from that
   rotvec(1:3) = x
